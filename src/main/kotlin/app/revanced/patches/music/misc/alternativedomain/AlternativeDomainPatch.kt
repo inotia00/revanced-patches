4231df225373abef7017abc1ac143d517a818eaf
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-07-14 11:51:47 +0900

    feat: apply code review suggestions

--- src/main/kotlin/app/revanced/patches/music/misc/alternativedomain/AlternativeDomainPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/misc/alternativedomain/AlternativeDomainPatch.kt
@@ -1,51 +1,22 @@
 package app.revanced.patches.music.misc.alternativedomain
 
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
-import app.revanced.patcher.fingerprint.MethodFingerprint
-import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import app.revanced.patches.music.misc.alternativedomain.fingerprints.MessageDigestImageUrlFingerprint
-import app.revanced.patches.music.misc.alternativedomain.fingerprints.MessageDigestImageUrlParentFingerprint
 import app.revanced.patches.music.utils.compatibility.Constants.COMPATIBLE_PACKAGE
-import app.revanced.patches.music.utils.integrations.Constants.ALTERNATIVE_DOMAIN_CLASS_DESCRIPTOR
 import app.revanced.patches.music.utils.settings.CategoryType
 import app.revanced.patches.music.utils.settings.SettingsPatch
 import app.revanced.util.patch.BaseBytecodePatch
-import app.revanced.util.resultOrThrow
 
 @Suppress("unused")
 object AlternativeDomainPatch : BaseBytecodePatch(
     name = "Alternative domain",
     description = "Adds options to replace static images(avatars, playlist covers, etc.) domain.",
     dependencies = setOf(
-        SettingsPatch::class,
+        AlternativeDomainBytecodePatch::class,
+        SettingsPatch::class
     ),
-    compatiblePackages = COMPATIBLE_PACKAGE,
-    fingerprints = setOf(
-        MessageDigestImageUrlParentFingerprint
-    )
+    compatiblePackages = COMPATIBLE_PACKAGE
 ) {
-
     override fun execute(context: BytecodeContext) {
-        fun MethodFingerprint.alsoResolve(fingerprint: MethodFingerprint) =
-            also { resolve(context, fingerprint.resultOrThrow().classDef) }.resultOrThrow()
-
-        fun MethodFingerprint.resolveAndLetMutableMethod(
-            fingerprint: MethodFingerprint,
-            block: (MutableMethod) -> Unit
-        ) = alsoResolve(fingerprint).also { block(it.mutableMethod) }
-
-        MessageDigestImageUrlFingerprint.resolveAndLetMutableMethod(
-            MessageDigestImageUrlParentFingerprint
-        ) {
-            it.addInstructions(
-                0,
-                """
-                invoke-static { p1 }, $ALTERNATIVE_DOMAIN_CLASS_DESCRIPTOR->overrideImageURL(Ljava/lang/String;)Ljava/lang/String;
-                move-result-object p1
-                """
-            )
-        }
 
         SettingsPatch.addSwitchPreference(
             CategoryType.MISC,
@@ -57,5 +28,6 @@ object AlternativeDomainPatch : BaseBytecodePatch(
             "revanced_alternative_domain",
             "revanced_use_alternative_domain"
         )
+
     }
 }
