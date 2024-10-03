37e803e8c1c3b86c72134b36d0aa85d59a64d6bb
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-10-03 21:34:01 +0900

    feat: instead of checking the opcode pattern, use the `injectLiteralInstructionViewCall` function

--- src/main/kotlin/app/revanced/patches/youtube/feed/components/fingerprints/ContentPillFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/feed/components/fingerprints/ContentPillFingerprint.kt
@@ -4,17 +4,10 @@ import app.revanced.patcher.extensions.or
 import app.revanced.patches.youtube.utils.resourceid.SharedResourceIdPatch.ContentPill
 import app.revanced.util.fingerprint.LiteralValueFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
-import com.android.tools.smali.dexlib2.Opcode
 
 internal object ContentPillFingerprint : LiteralValueFingerprint(
     returnType = "V",
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
     parameters = listOf("L", "Z"),
-    opcodes = listOf(
-        Opcode.CONST,
-        Opcode.IGET_OBJECT,
-        Opcode.INVOKE_VIRTUAL,
-        Opcode.MOVE_RESULT_OBJECT
-    ),
-    literalSupplier = { ContentPill }
+    literalSupplier = { ContentPill },
 )
